323e02bf2c88d21f012842405735216f93063d86
misioq23 <mi23chal@gmail.com>
2019-03-29 23:24:29 +0100

    added navbar scroll color change

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,17 @@
 const navbar = document.querySelector('.navbar');
 const burger = document.querySelector('.burger');
+const masthead = document.querySelector('.masthead');
+const navbarHeight = navbar.clientHeight;
+window.addEventListener('scroll', () => {
+	const mastheadHeight = masthead.scrollHeight - navbarHeight;
+
+	if (window.pageYOffset > mastheadHeight) {
+		navbar.classList.add('js-navbar--scroll');
+	} else {
+		navbar.classList.remove('js-navbar--scroll');
+	}
+});
+
 burger.addEventListener('click', () => {
 	navbar.classList.toggle('js-navbar__burger--active');
 });
