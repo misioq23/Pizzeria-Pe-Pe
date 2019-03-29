c10d559864d7fc538aaa499b7492e6300b5452ff
misioq23 <mi23chal@gmail.com>
2019-03-29 21:52:08 +0100

    Changes inside header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,5 @@
-const nav = document.querySelector('.nav');
-const navBurger = document.querySelector('.nav__burger');
-document.querySelector('.nav__btn').addEventListener('click', () => {
-	nav.classList.toggle('js-nav--active');
-	navBurger.classList.toggle('js-nav__burger--active');
+const navbar = document.querySelector('.navbar');
+const burger = document.querySelector('.burger');
+burger.addEventListener('click', () => {
+	navbar.classList.toggle('js-navbar__burger--active');
 });
