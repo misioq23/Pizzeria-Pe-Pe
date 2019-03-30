659ada4b457330efc1e9392b27f43d6df55f8673
misioq23 <mi23chal@gmail.com>
2019-03-30 20:48:03 +0100

    Improved performance in header  rendering

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,26 @@
 const navbar = document.querySelector('.navbar');
 const burger = document.querySelector('.burger');
 const masthead = document.querySelector('.masthead');
-const navbarHeight = navbar.clientHeight;
-window.addEventListener('scroll', () => {
+const navbarHeight = navbar.offsetHeight;
+
+function navbarScroll() {
 	const mastheadHeight = masthead.scrollHeight - navbarHeight;
 
-	if (window.pageYOffset > mastheadHeight) {
-		navbar.classList.add('js-navbar--scroll');
-	} else {
-		navbar.classList.remove('js-navbar--scroll');
-	}
+	window.addEventListener('scroll', () => {
+		if (window.pageYOffset > mastheadHeight) {
+			navbar.classList.add('js-navbar--scroll');
+		} else {
+			navbar.classList.remove('js-navbar--scroll');
+		}
+	});
+}
+
+window.addEventListener('resize', () => {
+	navbarScroll();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	navbarScroll();
 });
 
 burger.addEventListener('click', () => {
