2d8f7f62e3f9afce603dc6983a50efc84719f38f
misioq23 <mi23chal@gmail.com>
2019-04-04 23:25:26 +0200

    Parallax

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,18 +4,20 @@ import throttle from 'lodash/throttle';
 const navbar = document.querySelector('.navbar');
 const burger = document.querySelector('.burger');
 const hero = document.querySelector('.hero');
+const heroParallax = document.querySelector('.hero__parallax-img');
 const navbarHeight = navbar.offsetHeight;
 
 function navbarScroll() {
-	const heroHeight = hero.scrollHeight - navbarHeight;
+	const heroHeight = hero.getBoundingClientRect().height - navbarHeight;
 
 	window.addEventListener('scroll', throttle(() => {
-		if (window.pageYOffset > heroHeight) {
-			navbar.classList.add('js-navbar--scroll');
-		} else {
+		if (window.pageYOffset < heroHeight) {
 			navbar.classList.remove('js-navbar--scroll');
+			heroParallax.style.transform = `translateY(-${window.pageYOffset / 3}px)`;
+		} else {
+			navbar.classList.add('js-navbar--scroll');
 		}
-	}, 100));
+	}, 10));
 }
 
 window.addEventListener('resize', debounce(() => {
