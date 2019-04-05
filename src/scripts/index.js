700e152fbbd6e50a8dc00b05077d17f4bd160948
misioq23 <mi23chal@gmail.com>
2019-04-05 22:19:09 +0200

    Hero headline and button fading

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,18 +5,35 @@ const navbar = document.querySelector('.navbar');
 const burger = document.querySelector('.burger');
 const hero = document.querySelector('.hero');
 const heroParallax = document.querySelector('.hero__parallax');
+const headline = document.querySelector('.headline');
+const heroBtn = document.querySelector('.hero__btn--cta');
 const navbarHeight = navbar.offsetHeight;
 
 function navbarScroll() {
 	const heroHeight = hero.getBoundingClientRect().height - navbarHeight;
+	// Actual window position + headline distance to top of window + (visual improvment: half of headline height)
+	const headlineDistanceTop = (window.pageYOffset + headline.getBoundingClientRect().top + (headline.getBoundingClientRect().height) / 2);
+	const heroBtnDistanceTop = (window.pageYOffset + heroBtn.getBoundingClientRect().top);
+	const heroBtnCheckpoint = heroBtnDistanceTop / 2;
 
 	window.addEventListener('scroll', throttle(() => {
 		const actualPosition = window.pageYOffset;
 		if (actualPosition < heroHeight) {
 			navbar.classList.remove('js-navbar--scroll');
+			// Parallax
 			heroParallax.style.transform = `translateY(-${(actualPosition / 3).toFixed()}px)`;
 		} else {
 			navbar.classList.add('js-navbar--scroll');
+		}
+		// Fading Headline
+		actualPosition < headlineDistanceTop ? headline.style.opacity = `${(1 - (actualPosition / headlineDistanceTop)).toFixed(2)}` : headline.style.opacity = '0';
+		// Fading Call to Action
+		if (actualPosition > heroBtnDistanceTop) {
+			heroBtn.style.opacity = '0';
+		} else if (actualPosition > heroBtnCheckpoint) {
+			heroBtn.style.opacity = `${(1 - ((actualPosition - heroBtnCheckpoint) / heroBtnCheckpoint)).toFixed(2)}`;
+		} else {
+			heroBtn.style.opacity = '1';
 		}
 	}, 10));
 }
