22ebf29a4a9144c7a7dae4b3b668a17e5855ddff
misioq23 <mi23chal@gmail.com>
2019-04-08 19:26:15 +0200

    scrollEvents improvments, opacityDecrease f. added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,15 +2,23 @@ import debounce from 'lodash/debounce';
 import throttle from 'lodash/throttle';
 import config from './config';
 
+function opacityDecrease(actualPosition, elementDistanceFromTop, startFadingPosition = 0) {
+	const opacity = (1 - ((actualPosition - startFadingPosition) / (elementDistanceFromTop - startFadingPosition))).toFixed(2);
+	// prevent e.g. opacity = 1.05;
+	return Math.min(1, opacity);
+}
+
 function navbarScroll() {
-	const heroEndCheckpoint = config.DOM.hero.getBoundingClientRect().height - config.DOM.navbar.offsetHeight;
+	const heroEndCheckpoint = config.DOM.hero.offsetHeight - config.DOM.navbar.offsetHeight;
 	// Actual window position + headline distance to top of window + (visual improvment: half of headline height)
-	const headlineDistanceTop = (window.pageYOffset + config.DOM.headline.getBoundingClientRect().top + (config.DOM.headline.getBoundingClientRect().height) / 2);
-	const heroBtnDistanceTop = (window.pageYOffset + config.DOM.heroBtn.getBoundingClientRect().top);
-	const heroBtnCheckpoint = heroBtnDistanceTop / 2;
+	const headlineDistanceTop = window.pageYOffset + config.DOM.headline.getBoundingClientRect().top + (config.DOM.headline.offsetHeight / 2);
+	const heroBtnDistanceTop = window.pageYOffset + config.DOM.heroBtn.getBoundingClientRect().top;
+	const heroBtnFadeStartPosition = heroBtnDistanceTop / 2;
 
 	const scrollEvents = () => {
 		const actualPosition = window.pageYOffset;
+		
+		// Navbar change color & Parallax
 		if (actualPosition < heroEndCheckpoint) {
 			config.DOM.navbar.classList.remove('js-navbar--scroll');
 			// Parallax
@@ -18,16 +26,12 @@ function navbarScroll() {
 		} else {
 			config.DOM.navbar.classList.add('js-navbar--scroll');
 		}
+
 		// Fading Headline
-		actualPosition < headlineDistanceTop ? config.DOM.headline.style.opacity = `${(1 - (actualPosition / headlineDistanceTop)).toFixed(2)}` : config.DOM.headline.style.opacity = '0';
+		actualPosition < headlineDistanceTop ? config.DOM.headline.style.opacity = `${opacityDecrease(actualPosition, headlineDistanceTop)}` : config.DOM.headline.style.opacity = '0';
+
 		// Fading Call to Action
-		if (actualPosition > heroBtnDistanceTop) {
-			config.DOM.heroBtn.style.opacity = '0';
-		} else if (actualPosition > heroBtnCheckpoint) {
-			config.DOM.heroBtn.style.opacity = `${(1 - ((actualPosition - heroBtnCheckpoint) / heroBtnCheckpoint)).toFixed(2)}`;
-		} else {
-			config.DOM.heroBtn.style.opacity = '1';
-		}
+		actualPosition < heroBtnDistanceTop ? config.DOM.heroBtn.style.opacity = `${opacityDecrease(actualPosition, heroBtnDistanceTop, heroBtnFadeStartPosition)}` : config.DOM.heroBtn.style.opacity = '0';
 	};
 
 	window.addEventListener('scroll', throttle(scrollEvents, 10));
