a930f170a73e587ceebd0d5d9e6e1f5aee0f9dcc
misioq23 <mi23chal@gmail.com>
2019-04-08 19:22:05 +0200

    Added config file with DOM consts

---
+++ src/scripts/config.js
@@ -0,0 +1,10 @@
+export default {
+	DOM: {
+		navbar: document.querySelector('.navbar'),
+		burger: document.querySelector('.burger'),
+		hero: document.querySelector('.hero'),
+		heroParallax: document.querySelector('.hero__parallax'),
+		headline: document.querySelector('.headline'),
+		heroBtn: document.querySelector('.hero__btn--cta'),
+	}
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,39 +1,32 @@
 import debounce from 'lodash/debounce';
 import throttle from 'lodash/throttle';
-
-const navbar = document.querySelector('.navbar');
-const burger = document.querySelector('.burger');
-const hero = document.querySelector('.hero');
-const heroParallax = document.querySelector('.hero__parallax');
-const headline = document.querySelector('.headline');
-const heroBtn = document.querySelector('.hero__btn--cta');
-const navbarHeight = navbar.offsetHeight;
+import config from './config';
 
 function navbarScroll() {
-	const heroHeight = hero.getBoundingClientRect().height - navbarHeight;
+	const heroEndCheckpoint = config.DOM.hero.getBoundingClientRect().height - config.DOM.navbar.offsetHeight;
 	// Actual window position + headline distance to top of window + (visual improvment: half of headline height)
-	const headlineDistanceTop = (window.pageYOffset + headline.getBoundingClientRect().top + (headline.getBoundingClientRect().height) / 2);
-	const heroBtnDistanceTop = (window.pageYOffset + heroBtn.getBoundingClientRect().top);
+	const headlineDistanceTop = (window.pageYOffset + config.DOM.headline.getBoundingClientRect().top + (config.DOM.headline.getBoundingClientRect().height) / 2);
+	const heroBtnDistanceTop = (window.pageYOffset + config.DOM.heroBtn.getBoundingClientRect().top);
 	const heroBtnCheckpoint = heroBtnDistanceTop / 2;
 
 	const scrollEvents = () => {
 		const actualPosition = window.pageYOffset;
-		if (actualPosition < heroHeight) {
-			navbar.classList.remove('js-navbar--scroll');
+		if (actualPosition < heroEndCheckpoint) {
+			config.DOM.navbar.classList.remove('js-navbar--scroll');
 			// Parallax
-			heroParallax.style.transform = `translateY(-${(actualPosition / 3).toFixed()}px)`;
+			config.DOM.heroParallax.style.transform = `translateY(-${(actualPosition / 3).toFixed()}px)`;
 		} else {
-			navbar.classList.add('js-navbar--scroll');
+			config.DOM.navbar.classList.add('js-navbar--scroll');
 		}
 		// Fading Headline
-		actualPosition < headlineDistanceTop ? headline.style.opacity = `${(1 - (actualPosition / headlineDistanceTop)).toFixed(2)}` : headline.style.opacity = '0';
+		actualPosition < headlineDistanceTop ? config.DOM.headline.style.opacity = `${(1 - (actualPosition / headlineDistanceTop)).toFixed(2)}` : config.DOM.headline.style.opacity = '0';
 		// Fading Call to Action
 		if (actualPosition > heroBtnDistanceTop) {
-			heroBtn.style.opacity = '0';
+			config.DOM.heroBtn.style.opacity = '0';
 		} else if (actualPosition > heroBtnCheckpoint) {
-			heroBtn.style.opacity = `${(1 - ((actualPosition - heroBtnCheckpoint) / heroBtnCheckpoint)).toFixed(2)}`;
+			config.DOM.heroBtn.style.opacity = `${(1 - ((actualPosition - heroBtnCheckpoint) / heroBtnCheckpoint)).toFixed(2)}`;
 		} else {
-			heroBtn.style.opacity = '1';
+			config.DOM.heroBtn.style.opacity = '1';
 		}
 	};
 
@@ -48,6 +41,6 @@ window.addEventListener('resize', debounce(() => {
 	navbarScroll();
 }, 500));
 
-burger.addEventListener('click', () => {
-	navbar.classList.toggle('js-navbar__burger--active');
+config.DOM.burger.addEventListener('click', () => {
+	config.DOM.navbar.classList.toggle('js-navbar__burger--active');
 });
