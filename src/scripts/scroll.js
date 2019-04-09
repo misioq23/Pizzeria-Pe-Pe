e8a48b24c06d63f62472bd07c23897bade589e6c
misioq23 <mi23chal@gmail.com>
2019-04-09 09:53:47 +0200

    Refresh navbar and hero after resize

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -7,24 +7,29 @@ function opacityDecrease(actualPosition, elementDistanceFromTop, startFadingPosi
 	return Math.min(1, opacity);
 }
 
+function refreshHeader(actualPosition, checkpoint) {
+	if (actualPosition < checkpoint) {
+		config.DOM.navbar.classList.remove('js-navbar--scroll');
+		// Parallax
+		config.DOM.heroParallax.style.transform = `translateY(-${(actualPosition / 3).toFixed()}px)`;
+	} else {
+		config.DOM.navbar.classList.add('js-navbar--scroll');
+	}
+}
+
 export default function navbarScroll() {
+	const actualPosition = window.pageYOffset;
 	const heroEndCheckpoint = config.DOM.hero.offsetHeight - config.DOM.navbar.offsetHeight;
 	// Actual window position + headline distance to top of window + (visual improvment: half of headline height)
-	const headlineDistanceTop = window.pageYOffset + config.DOM.headline.getBoundingClientRect().top + (config.DOM.headline.offsetHeight / 2);
-	const heroBtnDistanceTop = window.pageYOffset + config.DOM.heroBtn.getBoundingClientRect().top;
+	const headlineDistanceTop = actualPosition + config.DOM.headline.getBoundingClientRect().top + (config.DOM.headline.offsetHeight / 2);
+	const heroBtnDistanceTop = actualPosition + config.DOM.heroBtn.getBoundingClientRect().top;
 	const heroBtnFadeStartPosition = heroBtnDistanceTop / 2;
-
+	refreshHeader(actualPosition, heroEndCheckpoint);
 	const scrollEvents = () => {
 		const actualPosition = window.pageYOffset;
 
 		// Navbar change color & Parallax
-		if (actualPosition < heroEndCheckpoint) {
-			config.DOM.navbar.classList.remove('js-navbar--scroll');
-			// Parallax
-			config.DOM.heroParallax.style.transform = `translateY(-${(actualPosition / 3).toFixed()}px)`;
-		} else {
-			config.DOM.navbar.classList.add('js-navbar--scroll');
-		}
+		refreshHeader(actualPosition, heroEndCheckpoint);
 
 		// Fading Headline
 		actualPosition < headlineDistanceTop ? config.DOM.headline.style.opacity = `${opacityDecrease(actualPosition, headlineDistanceTop)}` : config.DOM.headline.style.opacity = '0';
