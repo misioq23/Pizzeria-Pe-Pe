e1dfbb92d91e8e619c64febcc1fc1bf4d0b9e91d
misioq23 <mi23chal@gmail.com>
2019-04-23 17:37:46 +0200

    scrollSpy edited

--- src/scripts/functions/scrollSpy.js
+++ src/scripts/functions/scrollSpy.js
@@ -1,21 +1,22 @@
 import { elements, elementStrings } from '../config';
-function scrollSpyFactory() {
-	const data = {};
-	data.btnArr = Array.prototype.slice.call(elements.navLinks);
-	data.targetArr = data.btnArr.map((el) => {
+function scrollSpyFactory(spiedElementsCollection) {
+	const data = {
+		elements: Array.prototype.slice.call(spiedElementsCollection),
+	};
+	data.targets = data.elements.map((el) => {
 		return `#${el.dataset.target}`;
 	});
 	return {
 		spy(actualSection) {
-			const index = data.targetArr.indexOf(actualSection);
-			if (data.actual) {
-				data.actual.classList.remove(elementStrings.navLinkActive);
+			const index = data.targets.indexOf(actualSection);
+			if (data.previous) {
+				data.previous.classList.remove(elementStrings.navLinkActive);
 			}
 			if (index !== -1) {
-				data.btnArr[index].classList.add(elementStrings.navLinkActive);
-				data.actual = data.btnArr[index];
+				data.elements[index].classList.add(elementStrings.navLinkActive);
+				data.previous = data.elements[index];
 			}
 		}
 	};
 }
-export const scrollSpy = scrollSpyFactory();
+export const scrollSpy = scrollSpyFactory(elements.navLinks);
