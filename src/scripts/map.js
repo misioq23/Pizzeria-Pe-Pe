30c8a6645d29a03d36ccb0b72344084284665c0a
misioq23 <mi23chal@gmail.com>
2019-05-05 22:23:27 +0200

    bundle improved; split map.js;

---
+++ src/scripts/map.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+(function() {
+	var pepeLoc = [52.2165, 21.2342];
+	var pepe = L.map(document.querySelector('.map'), {
+		dragging: !L.Browser.mobile
+	}).setView(pepeLoc, 16);
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+		maxZoom: 18,
+		id: 'mapbox.streets',
+		accessToken: 'pk.eyJ1IjoibWkyM2NoYWwiLCJhIjoiY2p1eWY2ZGdpMGhkOTQzcGlqYzB3Mm9zdyJ9.0TBjab2egaDEHuROAQE_0Q'
+	}).addTo(pepe);
+	L.marker(pepeLoc).addTo(pepe);
+})();
