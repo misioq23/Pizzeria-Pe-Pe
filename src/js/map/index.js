02c734f6885176ddfdaa1f7a64f5f03fe15002d8
Michał <michal.pawluczuk@escola.pl>
2022-10-14 19:39:42 +0200

    Fix map & address

--- src/js/map/index.js
+++ src/js/map/index.js
@@ -1,15 +1,14 @@
 import L from 'leaflet';
 
 const map = () => {
-	const pepeLoc = [52.2165, 21.2342];
-	const pepe = L.map(document.querySelector('.map'), { dragging: !L.Browser.mobile }).setView(pepeLoc, 16);
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	const pepeLoc = [52.2176335306207, 21.22334557747987];
+	const pepe = L.map(document.querySelector('.map'), {
+		dragging: !L.Browser.mobile,
+	}).setView(pepeLoc, 16);
+	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 18,
-		id: 'mapbox.streets',
-		accessToken: 'pk.eyJ1IjoibWkyM2NoYWwiLCJhIjoiY2p1eWY2ZGdpMGhkOTQzcGlqYzB3Mm9zdyJ9.0TBjab2egaDEHuROAQE_0Q',
 	}).addTo(pepe);
 	L.marker(pepeLoc).addTo(pepe);
 };
 
 export default map;
-
